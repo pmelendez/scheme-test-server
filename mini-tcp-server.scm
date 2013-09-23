9757c309b2f7ae393c6e9a4bd7ffe85fa437a834
Pedro <pmelendez@pevicom.com>
2013-09-23 09:04:12 -0400

    Initial commit

---
+++ mini-tcp-server.scm
@@ -0,0 +1,54 @@
+(require-extension tcp posix srfi-18)
+
+(define host "127.0.0.1")
+(define port 14095)
+
+
+(define listener (tcp-listen port))
+(define socket-list '())
+(define message-queue '())
+
+(define accept-handler (lambda()
+			 (begin
+			   (define-values (in out) (tcp-accept listener))
+			   (set! socket-list (append socket-list (cons (cons in out) '())))
+			   (accept-handler)
+			   )))
+
+(define send-message (lambda ()
+		       (letrec ((send-to (lambda(slist)
+					   (begin
+					     (if (not (null? slist)) (write-line "Hola" (cdar slist) ) #t)
+					     (thread-sleep! 1)
+					     (if (null? slist) (send-to socket-list) (send-to (cdr slist)))))))
+			 (send-to socket-list))))
+
+#|(define read-messages 
+  (lambda ()
+  (letrec ((inner  (lambda ()
+			(letrec ((read-from (lambda (socketlist)
+					      (let ((message (read (caar socketlist))))
+						(set! message-queue (append message-queue (list message))))
+					      (if (not (null? socketlist)) 
+						(read-from (cdr socketlist)) 
+						(read-from socket-list)))))
+			  (read-from socket-list)))))
+    (thread-start! (make-thread inner)))))
+|#						
+
+(define accept (lambda () (let ((thread (make-thread 
+					  accept-handler)))
+			    ;(thread-join! (thread-start! thread)))))
+			    (thread-start! thread))))
+
+(define send-messages (lambda () (let ((thread (make-thread
+						 send-message)))
+				   (thread-start! thread))))
+
+(define main (lambda () 
+	       (main)))
+
+(accept)
+(send-messages)
+;(read-messages)
+(main)
