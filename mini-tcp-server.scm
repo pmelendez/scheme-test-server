22ed31c99d515d047a895dc309f9a31d83f92d6e
Pedro <pmelendez@pevicom.com>
2013-09-26 09:16:06 -0400

    Work in progress - Delivering messages

--- mini-tcp-server.scm
+++ mini-tcp-server.scm
@@ -1,4 +1,4 @@
-(require-extension tcp posix srfi-18)
+(require-extension tcp srfi-18)
 
 (define host "127.0.0.1")
 (define port 14095)
@@ -16,50 +16,69 @@
 			   (accept-handler)
 			   )))
 
-(define send-message (lambda ()
-		       (letrec ((send-to (lambda(slist)
-					   (begin
-					     (display "Sending message to ") (display slist) (display "\n")
-					     (if (not (null? slist)) (write-line "Hola\r\n" (cdar slist) ) #t)
-					     (thread-sleep! 0.5)
-					     (if (null? slist) (send-to socket-list) (send-to (cdr slist)))))))
-			 (send-to socket-list))))
+(define delivery-messages 
+	(lambda ()
+		(letrec ((inner-loop
+							 (lambda ()
+								 (letrec 
+									 ((local-send-message-to-all 
+											(lambda (message socketlist)
+												(if (not (null? socketlist))
+													(begin
+														;(display message)
+														(write-line message (cdar socketlist))
+														(thread-sleep! 1)
+														(local-send-message-to-all message (cdr socketlist)))
+													(begin (display "done")
+																 #t)
+													)))
+										(local-handle-message 
+											(lambda (messages)
+												(if (not (null? messages))
+													(begin 
+														(local-send-message-to-all (car messages) socket-list)
+														(set! messages (cdr messages))
+														(local-handle-message messages))
+													(begin 
+														;(display message-queue)
+														(local-handle-message message-queue))
+													))))
+									 (local-handle-message message-queue)))))
+			(thread-start! (make-thread inner-loop)))))
+										 
 
 (define read-messages 
   (lambda ()
     (letrec ((inner  (lambda ()
 		       (letrec ((read-from (lambda (socketlist)
-					     (display "Entrando\n")
-					     (thread-sleep! 0.5)
+					     (thread-sleep! 1)
 					     (if (not (null? socketlist))
-					       (begin
-						 (display "before read\n") 
-						 (let ((message (read-line (caar socketlist))))
-						   (display message) (display "\n")
-						   (set! message-queue (append message-queue (list message)))
-						   (read-from (cdr socketlist)))
-						 )
-					       (read-from socket-list)))))
-			 (read-from socket-list)))))
-      (thread-start! (make-thread inner)))))
+					       (begin 
+									 (if (char? (peek-char (caar socketlist)))
+										 (let ((message (read-line (caar socketlist))))
+											 (display message) (display "\n")
+											 (set! message-queue (append message-queue (list message)))
+											 (read-from (cdr socketlist)))
+										 #f)
+									 )
+								 (read-from socket-list)))))
+						 (read-from socket-list)))))
+			(thread-start! (make-thread inner)))))
 
 (define accept (lambda () (let ((thread (make-thread 
 					  accept-handler)))
 			    ;(thread-join! (thread-start! thread)))))
 			    (thread-start! thread))))
 
-(define send-messages (lambda () (let ((thread (make-thread
-						 send-message)))
-				   (thread-start! thread))))
-
-(define debug-tick (lambda() (letrec ((deb (lambda () (thread-sleep! 1) (display "tick") (deb))))
+(define debug-tick (lambda() (letrec ((deb (lambda () (thread-sleep! 1) (display message-queue) (deb))))
 			       (thread-start! (make-thread deb)))))
 
 (define main (lambda () 
 	       (main)))
 
+;(tcp-read-timeout 5)
 (accept)
-(send-messages)
+(delivery-messages)
 (read-messages)
-(debug-tick)
+;(debug-tick)
 (main)
